28c3c77ac10b427da69b9196e6637d63304030e4
Donald Wu <chenggeuw0512@gmail.com>
2021-02-22 19:37:33 +1030

    route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express'); 
 const app = express(); 
 const flame = require('flame'); 
@@ -32,3 +33,29 @@ app.use('/api', apiRoute);
 app.listen(PORT, () => {
     console.log('server is running'); 
 })
+=======
+const express = require("express");
+const app = express();
+const flame = require("flame");
+// port to list on//
+const PORT = process.env.PORT || 8080;
+//routes//
+const reserveRoute = require("./");
+const tableRoute = require("./");
+const apiRoute = require("./");
+//middleware//
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+//return to homepage
+app.get("/", (req, res) => {
+  res.send("home");
+});
+//applying routers//
+app.use("/reserve", reserveRoute);
+app.use("/tables", tableRoute);
+app.use("/api", apiRoute);
+//call//
+app.listen(PORT, () => {
+  console.log("server is running");
+});
+>>>>>>> 1ccaa4c56d355c4343f75b1b8c9383c12427391f
