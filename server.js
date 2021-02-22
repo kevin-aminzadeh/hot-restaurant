191baabba790f3028e32326a4b3b90f4ed79aa8e
TopGek99 <a1767142@student.adelaide.edu.au>
2021-02-22 21:16:24 +1030

    added api routes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-
 const express = require('express'); 
 const app = express(); 
 const path = require('path');
 // port to list on//
 const PORT  = process.env.PORT || 8080;
 
+var tables = [];
+var waitlist = [];
 
 //return to homepage
 
@@ -15,6 +16,18 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')))
 app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
 app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
 
+app.get('/api/tables',(req,res) => res.json(tables));
+app.get('/api/waitlist',(req,res) => res.json(waitlist));
+
+app.post('/api/tables',(req,res) => {
+    const newTable = req.body;
+    if (tables.length < 5) {
+        tables.push(newTable);
+    } else {
+        waitlist.push(newTable);
+    }
+    res.json(newTable);
+});
 
 //call//
 app.listen(PORT, () => {
